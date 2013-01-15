e45ccc9407837a7a160dffeea9fa916d2954a494
yoann <yoann@yo-pc>
2013-01-15 15:41:06 +0100

    correct tap event

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -12,13 +12,24 @@ registerEvents: function() {
     var self = this;
     
 	 $(window).on('hashchange', $.proxy(this.route, this));
-     // ... if not: register mouse events instead
-     $('body').on('mousedown', 'a', function(event) {
-        $(event.target).addClass('tappable-active');
-     });
-     $('body').on('mouseup', 'a', function(event) {
-        $(event.target).removeClass('tappable-active');
-     });
+     // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('touchend', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+            $(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+            $(event.target).removeClass('tappable-active');
+        });
+    }
 },
 slidePage: function(page) {
     var currentPageDest,
